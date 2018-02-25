05ab74b130ce86bf665295117996ed862695d700
Hugh Saalmans <hugh.saalmans@gmail.com>
2018-02-25 11:16:35 +1100

    test code for no database added

--- postgres-scripts/xx-create-aws-export-file.sql
+++ postgres-scripts/xx-create-aws-export-file.sql
@@ -1,3 +1,18 @@
+
+-- output bounding boxes to a test file
+COPY (
+	WITH bboxes AS (
+		SELECT json_build_object('id', locality_pid, 'l', ST_XMin(ST_Envelope(geom))::numeric(7,4), 'b', ST_YMin(ST_Envelope(geom))::numeric(6,4), 'r', ST_XMax(ST_Envelope(geom))::numeric(7,4), 't', ST_YMax(ST_Envelope(geom))::numeric(6,4)) AS bbox
+		FROM admin_bdys_201802.locality_bdys_display
+	)
+	SELECT replace(replace(json_agg(bbox)::text, '{{', '[{'), '}}', '}]') FROM bboxes
+) TO '/Users/hugh.saalmans/tmp/locality-bdys-display-bboxes.txt';
+
+
+
+
+
+
 SELECT locality_pid, locality_name, postcode, state, ST_AsBinary(geom) AS geom FROM admin_bdys_201802.locality_bdys_display;
 
 
