354091cd269e07591ee539c67e30ac93e21e307a
Hugh Saalmans <hugh.saalmans@gmail.com>
2025-01-21 10:09:22 +1100

    removed redundant TODO comment

--- testing/web/flatgeobuf/xx_code_scraps.sh
+++ testing/web/flatgeobuf/xx_code_scraps.sh
@@ -1,5 +1,5 @@
 
 
 ogr2ogr -f FlatGeobuf -nlt PROMOTE_TO_MULTI \
-/Users/s57405/Downloads/locality-bdys-display-202411.fgb \
-/Users/s57405/Downloads/locality-bdys-display-202411-shapefile/locality-bdys-display-202411.shp
+/Users/s57405/Downloads/locality-bdys-display-202502.fgb \
+/Users/s57405/Downloads/locality-bdys-display-202502-shapefile/locality-bdys-display-202502.shp
