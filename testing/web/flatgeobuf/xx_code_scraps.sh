f8c6285585c4f7bc7c46411397dd52ec0b78828b
Hugh Saalmans <hugh.saalmans@gmail.com>
2026-02-03 10:46:42 +0100

    updates for 202602 release

--- testing/web/flatgeobuf/xx_code_scraps.sh
+++ testing/web/flatgeobuf/xx_code_scraps.sh
@@ -1,5 +1,5 @@
 
 
 ogr2ogr -f FlatGeobuf -nlt PROMOTE_TO_MULTI \
-/Users/s57405/Downloads/locality-bdys-display-202511.fgb \
-/Users/s57405/Downloads/locality-bdys-display-202511-shapefile/locality-bdys-display-202511.shp
+/Users/s57405/Downloads/locality-bdys-display-202602.fgb \
+/Users/s57405/Downloads/locality-bdys-display-202602-shapefile/locality-bdys-display-202602.shp
